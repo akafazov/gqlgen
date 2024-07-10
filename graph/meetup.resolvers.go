eee31f00194c70c2715d5c23e4fad1f96f443192
Angel Kafazov <akafazov@cst-bg.net>
2024-07-10 17:13:54 +0300

    Combile 'Users' and 'Meetups' schema and resolvers

--- graph/meetup.resolvers.go
+++ graph/meetup.resolvers.go
@@ -21,12 +21,40 @@ func (r *mutationResolver) CreateMeetup(ctx context.Context, input model.NewMeet
 	return meetups.CreateMeetup(input)
 }
 
+// CreateUser is the resolver for the createUser field.
+func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (*model.User, error) {
+	return meetups.CreateUser(ctx, input)
+}
+
 // Meetups is the resolver for the meetups field.
 func (r *queryResolver) Meetups(ctx context.Context) ([]*model.Meetup, error) {
 	return meetups.GetMeetups(ctx)
 }
 
+// Users is the resolver for the users field.
+func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
+	println("Users() resolver called")
+	return meetups.GetUsers(ctx)
+}
+
+// Meetups is the resolver for the meetups field.
+func (r *userResolver) Meetups(ctx context.Context, obj *model.User) ([]*model.Meetup, error) {
+	return meetups.CreateMeetupWithUser(ctx, obj)
+}
+
 // Meetup returns MeetupResolver implementation.
 func (r *Resolver) Meetup() MeetupResolver { return &meetupResolver{r} }
 
+// Mutation returns MutationResolver implementation.
+func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
+// User returns UserResolver implementation.
+func (r *Resolver) User() UserResolver { return &userResolver{r} }
+
 type meetupResolver struct{ *Resolver }
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+type userResolver struct{ *Resolver }
