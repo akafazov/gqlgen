752635e2bde6f78ec1f7a9964158cba4421b90d2
Desislav Arashev <darashev@cst-bg.net>
2024-07-11 18:57:31 +0300

    Fix

--- generate/generate.go
+++ generate/generate.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/99designs/gqlgen/api"
 	"github.com/99designs/gqlgen/codegen/config"
-	"github.com/akafazov/gqlgen/plugins/gqlgen_plugin"
+	"github.com/darashevcstbg/gqlgen/plugins/gqlgen_plugin"
 )
 
 func main() {
@@ -17,7 +17,7 @@ func main() {
 	}
 
 	err = api.Generate(cfg,
-		api.AddPlugin(gqlgen_plugin.New("graph/meetup.resolvers.go", "github.com/akafazov/gqlgen/graph/model")),
+		api.AddPlugin(gqlgen_plugin.New("graph/meetup.resolvers.go", "github.com/darashevcstbg/gqlgen/graph/model")),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
